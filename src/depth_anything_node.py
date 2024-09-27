813a6dbdade8236e4165e39a69e9fcc24057bfc5
3bdu1lah <a2053231@gmail.com>
2024-09-27 21:04:02 +0800

    metric model

--- src/depth_anything_node.py
+++ src/depth_anything_node.py
@@ -18,16 +18,26 @@ DEVICE = 'cuda' if torch.cuda.is_available() else 'mps' if torch.backends.mps.is
 model_configs = {
     'vits': {'encoder': 'vits', 'features': 64, 'out_channels': [48, 96, 192, 384]},
     'vitb': {'encoder': 'vitb', 'features': 128, 'out_channels': [96, 192, 384, 768]},
-    'vitl': {'encoder': 'vitl', 'features': 256, 'out_channels': [256, 512, 1024, 1024]},
-    'vitg': {'encoder': 'vitg', 'features': 384, 'out_channels': [1536, 1536, 1536, 1536]}
+    'vitl': {'encoder': 'vitl', 'features': 256, 'out_channels': [256, 512, 1024, 1024]}
 }
 
-encoder = 'vits' # or 'vits', 'vitb', 'vitg'
+encoder = 'vits' # or 'vits', 'vitb'
+dataset = 'hypersim' # 'hypersim' for indoor model, 'vkitti' for outdoor model
+max_depth = 20 # 20 for indoor model, 80 for outdoor model
+
+model = DepthAnythingV2(**{**model_configs[encoder], 'max_depth': max_depth})
 pkg = rospack.get_path('depth_anything_v2_ros')
-model = DepthAnythingV2(**model_configs[encoder])
-model.load_state_dict(torch.load(f'{pkg}/src/Depth_Anything_V2/checkpoints/depth_anything_v2_{encoder}.pth', map_location='cpu', weights_only=True))
+model.load_state_dict(torch.load(f'{pkg}/src/Depth_Anything_V2/checkpoints/depth_anything_v2_metric_{dataset}_{encoder}.pth', map_location='cpu'))
 model = model.to(DEVICE).eval()
 
+
+def translate(value, leftMin, leftMax, rightMin, rightMax):
+    # Figure out how 'wide' each range is
+    leftSpan = leftMax - leftMin
+    rightSpan = rightMax - rightMin
+    valueScaled = (value - leftMin) / leftSpan
+
+    return rightMin + (valueScaled * rightSpan)
 
 class image_converter:
 
@@ -67,11 +77,9 @@ class image_converter:
       print(e)
 
     depth = model.infer_image(cv_image)
-    # depth = (depth - depth.min()) / (depth.max() - depth.min()) * 255.0
-    depth = depth.astype(np.float32)
 
     # cv2.imshow("Image window", depth)
-    # cv2.waitKey(3)
+    # cv2.waitKey(3)S
     time_now = rospy.Time.now()
     img_msg = self.bridge.cv2_to_imgmsg(depth, "32FC1")
     img_msg.header.stamp = time_now
@@ -95,4 +103,3 @@ def main(args):
 
 if __name__ == '__main__':
     main(sys.argv)
-
